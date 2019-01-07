c7a1e14d188186ab6873f3bf1ca6fa1c5a9d3993
zyc <395748788@qq,com>
2019-01-07 17:00:57 +0800

    修改了

--- git_test/src/main/java/GitTest.java
+++ git_test/src/main/java/GitTest.java
@@ -1,5 +1,8 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试GIT");
+        System.out.println("测试GIT");
+        System.out.println("测试GIT");
+        System.out.println("测试GIT");
     }
 }
